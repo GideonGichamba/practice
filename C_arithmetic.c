0cba3f3178c9cbeded57a9072e62a448750ca2a5
GideonGichamba <gideongichamba@gmail.com>
2022-07-04 02:30:58 -0700

    arithmetic practice

---
+++ C_arithmetic.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+	int a = 21;
+	int b = 10;
+	int c ;
+	
+	c = a + b;
+	printf("Line 1 - value of c will be %d\n", c);
+
+	c = a - b;
+	printf("Line 2 - value of c will be %d\n", c);
+
+	c = a * b;
+	printf("Line 3 - value of c will be %d\n", c);
+
+	c = a / b;
+	printf("Line 4 - value of c will be %d\n", c);
+
+	c = a % b;
+	printf("Line 5 - value of c will be %d\n", c);
+
+	c = a++;
+	printf("Line 6 - value of c is %d\n", c);
+
+	c = a--;
+	printf("Line 7 - value of c is %d\n", c);
+}
